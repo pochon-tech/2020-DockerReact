ff55c62dbae8172e67405286d8b0f41ef38f9f26
Apple <apple@appurunoMacBook-Pro.local>
2020-02-02 19:15:11 +0900

    proceed Component-HeaderComponent

--- app/src/js/components/Header.js
+++ app/src/js/components/Header.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Title from "./Header/Title";
 
 export default class Header extends React.Component {
   render() {
     return (
-      <div>Header</div>
+      <div><Title /></div>
     );
   }
 }
